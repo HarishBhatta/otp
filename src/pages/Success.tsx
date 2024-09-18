8d0b4bc1714dcb69c42ea9d461a08a65275bf99d
Harish <bhattaharish10@gmail.com>
2024-09-19 01:31:53 +0545

    Success Page

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,11 +1,11 @@
 const Success = () => {
   return (
     <div className="h-[100vh] flex justify-center items-center">
-      {/* <div className="h-40 w-80 rounded-lg flex justify-center items-center bg-green-200">
+      <div className="h-40 w-80 rounded-lg flex justify-center items-center bg-green-200">
         <span className="text-green-900 font-semibold">
           Your Otp Verification was successful!
         </span>
-      </div> */}
+      </div>
     </div>
   );
 };
